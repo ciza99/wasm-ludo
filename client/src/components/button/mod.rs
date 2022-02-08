d51420ca2a33b768c128be707e3acad7f06b93de
Michal Čížek <cizek@inqool.cz>
2022-02-08 18:48:50 +0100

    added toasts with anims

--- client/src/components/button/mod.rs
+++ client/src/components/button/mod.rs
@@ -11,6 +11,8 @@ pub struct ButtonProps {
   pub icon: Html,
   #[prop_or_default]
   pub class: String,
+  #[prop_or(String::from("bg-primary-600"))]
+  pub bg_color: String,
 }
 
 #[function_component(Button)]
@@ -21,11 +23,18 @@ pub fn button(props: &ButtonProps) -> Html {
     onclick,
     icon,
     class,
+    bg_color,
   } = props.clone();
 
+  let bg_color = if disabled {
+    "bg-neutral-600".into()
+  } else {
+    bg_color
+  };
+
   html! {
-    <button {onclick} {disabled} class={classes!(String::from("rounded bg-primary-600 hover:brightness-90 text-white
-      p-3 shadow-md font-semibold flex justify-center items-center gap-4"), disabled.then(|| "bg-neutral-600" ), class)}
+    <button {onclick} {disabled} class={classes!(String::from("rounded hover:brightness-90 text-white
+      p-3 shadow-md font-semibold flex justify-center items-center gap-4"), bg_color, class)}
     >
       { icon }
       { for children.iter() }
